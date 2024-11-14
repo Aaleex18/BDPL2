aec7c2660a65a74ae8517101faf808ab492a6a5f
Aaleex18 <a.martinezmartin@edu.uah.es>
2024-11-14 19:38:49 +0100

    Update esquema.sql

--- esquema.sql
+++ esquema.sql
@@ -93,15 +93,23 @@ SET search_path='nombre del esquema o esquemas utilizados';
 
 CREATE SCHEMA IF NOT EXISTS pl2final;
 
-CREATE TABLE pl2final.canciones (
+CREATE TABLE IF NO EXISTS pl2final.usuarios (
+    nombre_completo TEXT NOT NULL,
+	nombre_usuario TEXT UNIQUE NOT NULL,
+	email TEXT NOT NULL,
+	password TEXT NOT NULL,
+    CONSTRAINT usuario_pk PRIMARY KEY (nombre_usuario)
+);
+
+CREATE TABLE IF NO EXISTS pl2final.canciones (
     id_del_disco INTEGER NOT NULL,
     titulo_de_la_cancion TEXT NOT NULL,
-    duracion TIME NOT NULL;
+    duracion TIME NOT NULL; --falta lo del formato hh:mm:ss
     CONSTRAINT canciones_pk PRIMARY KEY (titulo_de_la_cancion),
     CONSTRAINT canciones_fk FOREIGN KEY (id_del_disco) REFERENCES pl2final.discos (id_disco)
 );
 
-CREATE TABLE pl2final.discos (
+CREATE TABLE IF NO EXISTS pl2final.discos (
     id_disco INTEGER NOT NULL,
     nombre_del_disco TEXT NOT NULL,
     fecha_de_lanzamiento INTEGER NOT NULL,
@@ -110,10 +118,11 @@ CREATE TABLE pl2final.discos (
     url_del_grupo TEXT NOT NULL,
     url_portada TEXT NOT NULL,
     CONSTRAINT fecha_valida CHECK (fecha_de_lanzamiento > 0),
-    CONSTRAINT discos_pk PRIMARY KEY (fecha_de_lanzamiento, nombre_del_disco)
+    CONSTRAINT discos_pk PRIMARY KEY (fecha_de_lanzamiento, nombre_del_disco),
+    CONSTRAINT canciones_fk FOREIGN KEY (id_grupo) REFERENCES (pl2final.grupo)
 );
 
-CREATE TABLE pl2final.ediciones (
+CREATE TABLE IF NO EXISTS pl2final.ediciones (
     id_del_disco INTEGER NOT NULL,
     anno_de_la_edicion INTEGER NOT NULL,
     pais_de_la_edicion TEXT NOT NULL,
@@ -122,6 +131,29 @@ CREATE TABLE pl2final.ediciones (
     CONSTRAINT pais_valido CHECK (pais != 'None' AND pais_de_la_edicion != ''),
     CONSTRAINT ediciones_pk PRIMARY KEY (formato, anno_de_la_edicion, pais_de_la_edicion), 
     CONSTRAINT ediciones_fk FOREIGN KEY (id_del_disco) REFERENCES pl2final.discos(id_disco)
+);
+
+CREATE TABLE IF NO EXISTS pl2final.generos (
+
+);
+
+CREATE TABLE IF NO EXISTS pl2final.grupo (
+
+);
+
+CREATE TABLE IF NO EXISTS pl2final.desea (
+    nombre_usuario TEXT NOT NULL ,
+    titulo_disco TEXT NOT NULL,
+    anno_lanzamiento_disco INTEGER NOT NULL,
+    CONSTRAINT anno_valido CHECK (anno_lanzamiento_disco > 0),
+    CONSTRAINT desea_fk FOREIGN KEY (nombre_usuario) REFERENCES pl2final.usuarios(nombre_usuarios),
+    CONSTRAINT desea_fk FOREIGN KEY (titulo_disco) REFERENCES pl2final.disco(nombre_del_disco),
+    CONSTRAINT desea_fk FOREIGN KEY (anno_lanzamiento_disco) REFERENCES pl2final.disco(fecha_de_lanzamiento)
+
+);
+
+CREATE TABLE IF NO EXISTS pl2final.tiene (
+
 );
 
 
